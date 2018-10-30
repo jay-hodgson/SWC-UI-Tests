f1efeac382e9ceb8b31d9489166410c386000309
Jay Hodgson <jayhodgson@HelloLaptop.local>
2018-10-30 11:22:56 -0700

    resize window before running test (dom elements are not found otherwise)

--- tests/browseproject.test.js
+++ tests/browseproject.test.js
@@ -12,11 +12,12 @@ console.time("full page load");
 
 fixture('DREAM Single Cell Transcriptomics Challenge page')
     .page('https://www.synapse.org/#!Synapse:syn15665609/wiki/582909')
+    .beforeEach(t => t.resizeWindow(1920, 1080))
 
 // test to get initial full page load (download app, run js, get project entity bundle, get wiki, process markdown)
 test(`Load DREAM Single Cell Transcriptomics Challenge page and go to Home Page`, async t => {
-    await t.setPageLoadTimeout(47000)
-            .expect(synapsePageSelectors.challengeImageWidget.visible).ok();
+    await t.setPageLoadTimeout(120000)
+    await t.expect(synapsePageSelectors.challengeImageWidget.visible).ok();
     console.timeEnd("full page load")
     console.time("go to home page");
     await t.click(synapsePageSelectors.images.nth(0))
